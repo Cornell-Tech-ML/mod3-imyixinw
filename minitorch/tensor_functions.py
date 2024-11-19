4fa474ddb1ca95e59732aa52c2798e8baa9a4b7c
imyixinw <wangyixin2002@gmail.com>
2024-11-18 23:17:35 -0500

    more debug print

--- minitorch/tensor_functions.py
+++ minitorch/tensor_functions.py
@@ -189,7 +189,7 @@ class Sum(Function):
         """Forward pass for the Sum function"""
         # HERE: debug for 3.3
         print("Sum forward")
-        print('a.backend ', a.backend)
+        print('a.backend ', a.backend.cuda)
         return a.f.add_reduce(a, int(dim.item()))
         # ctx.save_for_backward(a, dim)
         # if dim:
